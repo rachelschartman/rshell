c201671b9ae0a90935acd94e4307631ff26aa31e
rachelschartman1 <rachelschartman@gmail.com>
2016-08-12 03:58:24 +0000

    finished Cmd

--- Cmd.h
+++ Cmd.h
@@ -1,10 +1,29 @@
 #ifndef CMD_H
 #define CMD_H
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "BaseCmd.h"
 
-#include "BaseCmd"
+using namespace std;
 
-class CMD : public BaseCmd {
-
-
+class Cmd : public BaseCmd {
+    private: 
+        char** args;
+    public:
+        Cmd() { };
+        Cmd(char ** arg) : args(arg) { };
+        int execute() {
+            int procID, status;
+            procID = fork();
+            
+            if(procID == 0) {
+                int i = execvp(args[0], args);
+                exit(i);
+            }
+            int waitID = wait(&status);
+            return status;
+        };
 };
 #endif
